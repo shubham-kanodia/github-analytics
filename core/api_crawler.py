58f024b56fc4cd810e22f5f28b602afe7d211951
shubham-kanodia <shubhamkanodia123@gmail.com>
2022-06-08 18:09:34 +0530

    Add support for authenticated requests

--- core/api_crawler.py
+++ core/api_crawler.py
@@ -1,17 +1,28 @@
+import os
 import requests
 
 
 class ApiCrawler:
     HEADERS = {
-        "Accept": "application / vnd.github.v3 + json"
+        "Accept": "application / vnd.github.v3 + json",
+        "Authorization": f"token {os.environ['PAT']}"
     }
     BASE_URL = "https://api.github.com/repos/"
 
     def get_repo_data(self, repo_url: str):
         endpoint = self.BASE_URL + "/".join(repo_url.split("/")[-2:])
-        resp = requests.get(endpoint, headers=self.HEADERS).json()
-        return resp
+        resp = requests.get(endpoint, headers=self.HEADERS)
+
+        if resp.ok:
+            return resp.json()
+        else:
+            print(resp.json())
+            raise Exception("Request Failed")
 
     def call_url(self, url: str):
-        resp = requests.get(url, headers=self.HEADERS).json()
-        return resp
+        resp = requests.get(url, headers=self.HEADERS)
+        if resp.ok:
+            return resp.json()
+        else:
+            print(resp.json())
+            raise Exception("Request Failed")
--- main.py
+++ main.py
@@ -4,12 +4,20 @@ from core.data_collector import DataCollector
 data_collector = DataCollector()
 
 REPO_LIST_FILE_PATH = "data/best-of-crypto.txt"
+COMPLETED_LIST_FILE_PATH = "data/completed.txt"
+completed_repos = [_.strip("\n") for _ in open(COMPLETED_LIST_FILE_PATH, "r").readlines()]
+
 for line in open(REPO_LIST_FILE_PATH, "r").readlines():
     line = line.strip("\n")
-    repo_data = data_collector.collect(line)
 
-    json.dump(repo_data, open(
-        f"./data/repo/{repo_data['repo_full_name'].replace('/', '_')}.json",
-        "w"), indent=2)
+    if line in completed_repos:
+        continue
+    else:
+        repo_data = data_collector.collect(line)
 
-    break
+        json.dump(repo_data, open(
+            f"./data/repo/{repo_data['repo_full_name'].replace('/', '_')}.json",
+            "w"), indent=2)
+
+        print(f"Added repo - {repo_data['repo_full_name']}")
+        open(COMPLETED_LIST_FILE_PATH, "a").write(line + "\n")
